8b125a41572412be507292aceffcc140fe462a39
Rivan20 <95005451+Rivan20@users.noreply.github.com>
2022-09-01 00:03:57 +0700

    211511048_Muhammad Rivan Rivaldi

---
+++ Operator.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package minggu2;
+
+/**
+ *
+ * @author rivan
+ */
+public class Operator {
+    Class FloatingPoint{
+        public static void main(String[] args) {
+            double x = 92.98; 
+            int nx = (int) Math.round(x);
+        }
+    }
+}
